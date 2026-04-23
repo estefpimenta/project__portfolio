44347cc5936548ba99a3a22db5271e15f8fe0536
Estefanno <estefanno.pimenta@gmail.com>
2026-04-23 10:34:49 -0300

    update video footer

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,16 @@
 
 window.addEventListener('load', () => {
     const videoHeader = document.querySelector('.header__card-video');
+    const videoFooter = document.querySelector('.footer-video')
+
     videoHeader.src = './images/video/flowV2.mp4';
     videoHeader.autoplay = true;
     videoHeader.loop = true;
     videoHeader.muted = true;
+
+    videoFooter.src = './images/video/flowV2Inverted.mp4';
+    videoFooter.autoplay = true;
+    videoFooter.loop = true;
+    videoFooter.muted = true;
 })
 
