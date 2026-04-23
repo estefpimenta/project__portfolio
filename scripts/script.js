e1e301d9f32c1b0ade41cb99ae4ebe57eca89dc4
Estefanno <estefanno.pimenta@gmail.com>
2026-04-23 10:03:41 -0300

    update header img & video

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+// VIDEO HEADER - executado somento que depois que a janela é carregada
+
+window.addEventListener('load', () => {
+    const videoHeader = document.querySelector('.header__card-video');
+    videoHeader.src = './images/video/flowV2.mp4';
+})
+
